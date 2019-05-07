151ac098ccb5a7195de63b76e6d0569cc16affa0
Deno G <dguennoc@gmail.com>
2019-05-07 19:12:23 +0200

    7.31.1 et 7.32
    7.31.1 : Classe ItemList : création d'une nouvelle classe qui gère la création et les modif des différentes listes d'items existantes (liste des items de chaque room et liste des items portés par le player)
    
    7.32 : (déjà fait) poid max fixé afin que le player ne puisse pas prendre tous les objets

---
+++ ItemList.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+/**
+ * Write a description of class ItemList here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ItemList
+{
+    // instance variables - replace the example below with your own
+    private HashSet<Item> aItemList;
+
+    /**
+     * Constructor for objects of class ItemList
+     */
+    public ItemList()
+    {
+        this.aItemList = new HashSet<Item>();
+    }
+    
+    public void addItem (final Item pItem)
+    {
+        this.aItemList.add(pItem);
+    }
+    
+    public void removeItem (final Item pItem)
+    {
+        this.aItemList.remove(pItem);
+    }
+    
+    public boolean emptyList ()
+    {
+        return this.aItemList.isEmpty();
+    }
+    
+    public String toStringList ()
+    {
+        String vItemList = "Items : ";
+        if (this.aItemList.isEmpty())
+            return "No item here" + ".\n";
+        else   
+            for(Item vItem : aItemList)
+            {
+                vItemList += vItem.getItemName() + " ";
+            }
+        return vItemList + ".\n";
+    }
+    
+    public boolean ItemExist (final String pItemName)
+    {
+        boolean vItemExist = false;
+        for(Item vItem : this.aItemList)
+        {
+            if (vItem.getItemName().equals(pItemName))
+            {
+                return vItemExist = true;
+            }
+        }
+        return vItemExist;
+    }
+    
+    public Item getItem (final String pItemName)
+    {
+        for(Item vItem : this.aItemList)
+        {
+            if (vItem.getItemName().equals(pItemName))
+            {
+                return vItem;
+            }
+        }
+        return null;
+    }
+    
+}
